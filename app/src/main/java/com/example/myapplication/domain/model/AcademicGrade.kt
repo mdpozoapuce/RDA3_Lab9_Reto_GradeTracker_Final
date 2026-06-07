47ae198047157b93cf1978391c1c9b1eb46f5f3b
Mateo Pozo <mdpozoa@puce.edu.ec>
2026-06-07 18:39:39 -0500

    feat: presentation layer and state management setup

--- app/src/main/java/com/example/myapplication/domain/model/AcademicGrade.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.domain.model
-
-data class AcademicGrade(
-    val id: String,
-    val activityName: String,
-    val subject: String,
-    val grade: Double
-)
---
+++ app/src/main/java/myapplication/data/repository/InMemoryGradeRepository.kt
@@ -0,0 +1,2 @@
+package myapplication.data.repository
+
